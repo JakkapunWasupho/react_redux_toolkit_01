b998da748c93df6c563b4aff94d2c747cc1a2976
phandev <jakkapun.ine07@gmail.com>
2022-09-08 16:33:35 +0700

    edit

--- src/store/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { userSlice } from "./storeSlice"
-
-export const store = configureStore({
-    reducer: {
-        userAdmin: userSlice
-    }
-})
